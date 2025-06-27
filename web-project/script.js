6b01f14c35c4720ebbf72e6028b8d753a63f22aa
TheGandalfcoder <j4jos123@gmail.com>
2025-06-27 12:15:42 +0100

    Delete web-project directory

--- web-project/script.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: ["./**/*.html", "./**/*.js"], // Adjust to your project
-    darkMode: 'class', // ✅ Required for class-based dark mode
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
--- web-project/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: 'class', // Enables manual dark mode via 'class'
-  content: ['./**/*.html'], // Adjust this to match your project's HTML or component file paths
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}; 
